38890011792b3f44ad13317e9eb128a1751881ee
guoxuanhan <736071985@qq.com>
2024-05-06 16:02:21 +0800

    导入fairygui v5.1.0版本；导入fairygui-dynamic库 84ff638版本

---
+++ Unity/Assets/Scripts/Editor/Plugins/FairyGUI/CodeSpawn/FUIPackageHelperSpawner.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.IO;
+using Luban;
+using FairyGUI.Dynamic;
+
+namespace FUIEditor
+{
+    public static class FUIPackageHelperSpawner
+    {
+        public static void GenerateMappingFile()
+        {
+            var ids = new List<string>();
+            var names = new List<string>();
+            foreach (PackageInfo packageInfo in FUICodeSpawner.PackageInfos.Values)
+            {
+                ids.Add(packageInfo.Id);
+                names.Add(packageInfo.Name);
+            }
+
+            ByteBuf byteBuf = new ByteBuf();
+            byteBuf.WriteInt(ids.Count);
+            for (int i = 0; i < ids.Count; i++)
+            {
+                byteBuf.WriteString(ids[i]);
+                byteBuf.WriteString(names[i]);
+            }
+
+            string filePath = $"../Unity/Assets/Bundles/FUI/UIPackageMapping.bytes";
+            using FileStream fs = new FileStream(filePath, FileMode.Create);
+            fs.Write(byteBuf.Bytes, 0, byteBuf.Bytes.Length);
+        }
+    }
+}
