a7a95d83b725ff90c3f164b57415cce45e4e91f0
gxh_1995 <736071985@qq.com>
2024-09-03 21:37:01 +0800

    枚举帮助类

--- Unity/Assets/Scripts/Core/Helper/EnumHelper.cs
+++ Unity/Assets/Scripts/Core/Helper/EnumHelper.cs
@@ -2,29 +2,42 @@
 
 namespace ET
 {
-	public static class EnumHelper
-	{
-		public static int EnumIndex<T>(int value)
-		{
-			int i = 0;
-			foreach (object v in Enum.GetValues(typeof (T)))
-			{
-				if ((int) v == value)
-				{
-					return i;
-				}
-				++i;
-			}
-			return -1;
-		}
-
-		public static T FromString<T>(string str)
-		{
-            if (!Enum.IsDefined(typeof(T), str))
+    public static class EnumHelper
+    {
+        public static int EnumIndex<T>(int value)
+        {
+            int i = 0;
+            foreach (object v in Enum.GetValues(typeof (T)))
             {
-                return default(T);
+                if ((int)v == value)
+                {
+                    return i;
+                }
+
+                ++i;
             }
-            return (T)Enum.Parse(typeof(T), str);
+
+            return -1;
+        }
+
+        public static T FromString<T>(string str)
+        {
+            if (!Enum.IsDefined(typeof (T), str))
+            {
+                return default (T);
+            }
+
+            return (T)Enum.Parse(typeof (T), str);
+        }
+
+        public static bool IsDefine<T>(int key)
+        {
+            return Enum.IsDefined(typeof (T), key);
+        }
+
+        public static int ToInt(this Enum e)
+        {
+            return e.GetHashCode();
         }
     }
 }
