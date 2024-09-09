cd26cb059c585ac1f94ac8851de99e76be4db379
gxh_1995 <736071985@qq.com>
2024-09-09 22:14:56 +0800

    对象池实例化字典

---
+++ Unity/Assets/Scripts/Core/DictionaryComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ET
+{
+    public class DictionaryComponent<T, K>: Dictionary<T, K>, IDisposable
+    {
+        public static DictionaryComponent<T, K> Create()
+        {
+            return ObjectPool.Instance.Fetch(typeof (DictionaryComponent<T, K>)) as DictionaryComponent<T, K>;
+        }
+
+        public void Dispose()
+        {
+            this.Clear();
+            ObjectPool.Instance.Recycle(this);
+        }
+    }
+}
