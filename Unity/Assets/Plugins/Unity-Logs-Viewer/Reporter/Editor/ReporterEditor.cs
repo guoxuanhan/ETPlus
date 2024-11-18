1d5add12ee0ac34515f33cc166ce6a9047b8e875
gxh_1995 <736071985@qq.com>
2024-11-18 22:56:56 +0800

    修改log-viewer的创建菜单栏位置，让主界面更简洁

--- Unity/Assets/Plugins/Unity-Logs-Viewer/Reporter/Editor/ReporterEditor.cs
+++ Unity/Assets/Plugins/Unity-Logs-Viewer/Reporter/Editor/ReporterEditor.cs
@@ -8,7 +8,7 @@ using System.Collections;
 
 public class ReporterEditor : Editor
 {
-	[MenuItem("Reporter/Create")]
+	[MenuItem("Tools/Create Reporter")]
 	public static void CreateReporter()
 	{
 		const int ReporterExecOrder = -12000;
